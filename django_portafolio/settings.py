8e7df8a903b5904efc66ba0192b851b6556fa5c8
Pablo Alvarado <paalvarador@gmail.com>
2024-06-02 00:52:04 -0500

    Agregando el modelo de Admin en cada uno de los modelos del portafolio

--- django_portafolio/settings.py
+++ django_portafolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'portfolio'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guayaquil'
 
 USE_I18N = True
 
--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
+from .models import Profile, Project, Skill, Experience, Education, Contact
+
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    fields = ['name', 'bio', 'profile_picture', 'email', 'phone', 'linkedin', 'github', 'x', 'personal_website']
+
+    list_display = ['name', 'email', 'phone']
+
+admin.site.register(Profile, ProfileAdmin)
+
+class ProjectAdmin(admin.ModelAdmin):
+    fields = ['title', 'description', 'image', 'technologies', 'github_link', 'live_demo']
+
+    list_display = ['title', 'technologies', 'live_demo', 'created_at']
+
+admin.site.register(Project, ProjectAdmin)
+
+class SkillAdmin(admin.ModelAdmin):
+    fields = ['name', 'proficiency']
+
+    list_display = ['name', 'proficiency']
+
+admin.site.register(Skill, SkillAdmin)
+
+class ExperienceAdmin(admin.ModelAdmin):
+    fields = ['title', 'company', 'description', 'start_date', 'end_date']
+
+    list_display = ['title', 'company', 'start_date', 'end_date']
+
+admin.site.register(Experience, ExperienceAdmin)
+
+class EducationAdmin(admin.ModelAdmin):
+    fields = ['degree', 'institution', 'start_date', 'end_date', 'description']
+
+    list_display = ['degree', 'institution', 'start_date', 'end_date']
+
+admin.site.register(Education, EducationAdmin)
+
+class ContactAdmin(admin.ModelAdmin):
+    fields = ['name', 'email', 'message', 'sent_at']
+
+    list_display = ['name', 'email']
+
+admin.site.register(Contact, ContactAdmin)
