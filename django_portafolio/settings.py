2f4f1a4e4f71fe6ed4531dc4a697c46ed97c5a2d
Pablo Alvarado <paalvarador@gmail.com>
2024-06-12 00:05:54 -0500

    feat: Cambiando la base de datos de Sqlite3 a Postgresql

--- django_portafolio/settings.py
+++ django_portafolio/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+import dj_database_url
 
 load_dotenv();
 
@@ -83,10 +84,15 @@ WSGI_APPLICATION = 'django_portafolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+    'default': dj_database_url.config(
+        default=f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
