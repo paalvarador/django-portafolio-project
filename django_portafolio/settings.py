a0b66016e9b5da6d707d3a686defdd8ab40fe5f0
Pablo Alvarado <paalvarador@gmail.com>
2024-06-12 01:35:18 -0500

    fix: Tratando de solucionar el problema de los archivos estaticos

--- django_portafolio/settings.py
+++ django_portafolio/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-h$5p6%vbi+yjg$2_f#muj#c)1#*ybeov5h)3#@)_4vtg6f!k*9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -135,12 +135,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'portfolio/static',
-    BASE_DIR / 'blog/static', 
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
